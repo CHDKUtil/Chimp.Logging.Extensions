6196d51c4bccc7252cd0bb4c817d33a64a989614
Dmitry Shechtman <damnian@gmail.com>
2017-05-12 21:37:50 +0300

    Add solution

---
+++ src/NetStandard11/LoggerAdapter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Chimp.Logging.Extensions
+{
+    /// <summary>
+    /// Microsoft.Extensions.Logging logger adapter.
+    /// </summary>
+    public sealed class LoggerAdapter : ILogger
+    {
+        private Microsoft.Extensions.Logging.ILogger Logger { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggerAdapter"/> class.
+        /// </summary>
+        /// <param name="logger">Logger.</param>
+        public LoggerAdapter(Microsoft.Extensions.Logging.ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public void Log(LogLevel level, object obj)
+        {
+            Logger.Log(GetLogLevel(level), default(EventId), obj, null, null);
+        }
+
+        /// <inheritdoc/>
+        public void Log(LogLevel level, string str)
+        {
+            Logger.Log(GetLogLevel(level), default(EventId), str, null, null);
+        }
+
+        /// <inheritdoc/>
+        public void Log(LogLevel level, string format, params object[] args)
+        {
+            string str = string.Format(format, args);
+            Logger.Log(GetLogLevel(level), default(EventId), str, null, null);
+        }
+
+        /// <inheritdoc/>
+        private Microsoft.Extensions.Logging.LogLevel GetLogLevel(LogLevel level)
+        {
+            return (Microsoft.Extensions.Logging.LogLevel)level;
+        }
+    }
+}
---
+++ src/NetStandard11/LoggerFactoryAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Chimp.Logging.Extensions
+{
+    /// <summary>
+    /// Microsoft.Extensions.Logging logger factory adapter.
+    /// </summary>
+    public sealed class LoggerFactoryAdapter : ILoggerFactory
+    {
+        private Microsoft.Extensions.Logging.ILoggerFactory LoggerFactory { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggerFactoryAdapter"/> class.
+        /// </summary>
+        /// <param name="loggerFactory">Logger factory.</param>
+        public LoggerFactoryAdapter(Microsoft.Extensions.Logging.ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory;
+        }
+
+        /// <inheritdoc/>
+        public ILogger CreateLogger(string name)
+        {
+            var logger = LoggerFactory.CreateLogger(name);
+            return new LoggerAdapter(logger);
+        }
+
+        /// <inheritdoc/>
+        public ILogger CreateLogger(Type type)
+        {
+            var logger = LoggerFactory.CreateLogger(type);
+            return new LoggerAdapter(logger);
+        }
+
+        /// <inheritdoc/>
+        public ILogger CreateLogger<T>()
+        {
+            var logger = LoggerFactory.CreateLogger<T>();
+            return new LoggerAdapter(logger);
+        }
+    }
+}
