8204d486721693d15bd9056925d351949bf9f26d
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 18:56:53 +0300

    Add Dependency Injection

---
+++ src/NetStandard11/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Chimp.Logging.Extensions
+{
+    /// <summary>
+    /// <see cref="IServiceCollection"/> extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the Chimp.Logging adapter to the service collection.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection.</param>
+        /// <returns>Service collection.</returns>
+        public static IServiceCollection AddChimpLogging(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<ILoggerFactory, LoggerFactoryAdapter>();
+        }
+    }
+}
